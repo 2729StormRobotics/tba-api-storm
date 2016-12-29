6cc514f1a994570f60d9dd7de963e95e72e8f11d
atf1999 <torth212@gmail.com>
2016-12-29 15:09:09 -0500

    Fixed bugs

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const TBA = require('./tba-storm');
 
 var test = new TBA('1',1,'1');
 console.time('test');
-test.getTeam(2729).then(res => {
+test.getTeamEvents(2729, 2015).then(res => {
   console.log(res);
   console.timeEnd('test');
 }).catch(res => {
